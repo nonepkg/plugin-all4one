8f80b918d5d3cf07842475304dd29555e91a357a
jigsaw <j1g5awi@proton.me>
2023-06-02 22:25:37 +0800

    :arrow_up: update denpendencies

--- nonebot_plugin_all4one/__init__.py
+++ nonebot_plugin_all4one/__init__.py
@@ -3,11 +3,25 @@ from copy import deepcopy
 from nonebot import on, get_driver
 from nonebot.matcher import Matcher
 from nonebot.adapters import Bot, Event
+from nonebot.plugin import PluginMetadata
 from nonebot.exception import IgnoredException
 from nonebot.message import run_preprocessor, event_preprocessor
 
 from .config import Config
 from .onebotimpl import OneBotImplementation
+
+__plugin_meta__ = PluginMetadata(
+    name="OneBot 实现",
+    description="让 NoneBot2 成为 OneBot 实现！",
+    usage="""obimpl_connections = [{"type":"websocket_rev","url":"ws://127.0.0.1:8080/onebot/v12/"},{"type":"websocket_rev","url":"ws://127.0.0.1:4000/onebot/v12/", "self_id_prefix": "True"}] # 其它连接方式的配置同理
+middlewares = ["OneBot V11"] # 自定义加载的 Middleware，默认加载全部
+block_event = False # 是否中止已转发 Event 的处理流程，默认中止
+blocked_plugins = ["echo"] # 在 block_event=False 时生效，可自定义处理流程中要中止的插件""",
+    type="application",
+    homepage="https://github.com/nonepkg/nonebot-plugin-all4one",
+    config=Config,
+    supported_adapters={"~console", "~onebot.v11", "~telegram", "~qqguild"},
+)
 
 driver = get_driver()
 a4o_config = Config(**driver.config.dict())
