a616330ef886666d244c244ec7232ea918f8e75f
jigsaw <j1g5awi@proton.me>
2024-08-09 01:00:52 +0800

    :construction_worker: update docker files

--- docker/bot.py
+++ docker/bot.py
@@ -1,12 +1,14 @@
 import nonebot
-from nonebot.adapters.telegram import Adapter as TELEGRAMAdapter
-from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+from nonebot.adapters.discord import Adapter as DiscordAdapter
+from nonebot.adapters.telegram import Adapter as TelegramAdapter
+from nonebot.adapters.onebot.v11 import Adapter as OneBotV11Adapter
 
 nonebot.init()
 
 driver = nonebot.get_driver()
-driver.register_adapter(TELEGRAMAdapter)
-driver.register_adapter(ONEBOT_V11Adapter)
+driver.register_adapter(TelegramAdapter)
+driver.register_adapter(OneBotV11Adapter)
+driver.register_adapter(DiscordAdapter)
 
 nonebot.load_plugin("nonebot_plugin_sentry")
 nonebot.load_plugin("nonebot_plugin_all4one")
